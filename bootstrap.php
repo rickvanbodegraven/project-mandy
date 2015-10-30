2ebb3bdc43b18131e95abb0ad83631d70ff235c4
Rick van Bodegraven <rickvanbodegraven@gmail.com>
2015-10-30 16:35:17 +0100

    Did indeed decide to overhaul a massive part of the framework. This feels more robust.

---
+++ bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+require_once('Core/Autoloader.class.php');
+
+// register said autoloader
+$autoloader = new \Core\Autoloader();
+
+// instantiate the application handler now that we know where to find the class
+$application = new \Core\Application();
+
+// as early in the chain as possible, activate security functions and headers, before we process any user input
+$application->register(new \Core\Security())
+            ->register(new \Core\Session())  // start up session handling
+            ->register(new \Core\Routing()); // register router
+
+return $application;
--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
-error_reporting(E_ALL);
 
-// register our autoloader for class file resolution
-require_once('Core/Autoloader.class.php');
-$autoloader = new \Core\Autoloader();
-
-// as early in the chain as possible, activate security functions and headers, before we process any user input
-$security = new \Core\Security();
-
-// start up session handling
-$session = new \Core\Session();
-
-// register all known routes
-$router = new \Core\Routing();
+// bootstrap the application, load all of the modules and set up routing
+$application = require_once('bootstrap.php');
 
 try {
-    $output = $router->handleRequest();
+    $response = $application->handleRequest();
 } catch (\Exception $ex) {
     die($ex->getMessage());
 }
 
-echo $output;
+echo $response;
